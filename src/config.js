2f8587b067a4ce79f75a85edbfb4a08e78bac269
jaycapps <julian.capps@alchemysystems.com>
2020-05-19 16:51:11 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "julianhcapps-notes-app-bucket"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://hmmce71frd.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_8p2WMqnLO",
+      APP_CLIENT_ID: "73jhfgp8uvuhcbogtctuh77d90",
+      IDENTITY_POOL_ID: "us-east-1:d7a25001-f5c9-43ba-8bc7-a44027d215af"
+    }
+  };
